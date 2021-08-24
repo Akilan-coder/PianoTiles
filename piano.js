0913d69c18bfb3903be9085ff79c5fc66266cd8c
Akilan-coder <75996143+Akilan-coder@users.noreply.github.com>
2021-08-24 17:28:59 +0530

    Add files via upload

---
+++ piano.js
@@ -0,0 +1,5 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
