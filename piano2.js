dc5ff99067800b5d3af04328a993f886d4eef190
Akilan-coder <75996143+Akilan-coder@users.noreply.github.com>
2021-08-24 22:56:15 +0530

    Update piano2.js

--- piano2.js
+++ piano2.js
@@ -6,18 +6,18 @@ for (let j = 0; j < 12; j++) {
   keyaud[j] = new Audio();
 }
 
-keyaud[0].src = "c-3.mp3";
-keyaud[1].src = "d-3.mp3";
-keyaud[2].src = "f-3.mp3";
-keyaud[3].src = "g-3.mp3";
-keyaud[4].src = "a-3.mp3";
-keyaud[5].src = "c3.mp3";
-keyaud[6].src = "d3.mp3";
-keyaud[7].src = "e3.mp3";
-keyaud[8].src = "f3.mp3";
-keyaud[9].src = "g3.mp3";
-keyaud[10].src = "a4.mp3";
-keyaud[11].src = "b3.mp3";
+keyaud[0].src = "audio/c-3.mp3";
+keyaud[1].src = "audio/d-3.mp3";
+keyaud[2].src = "audio/f-3.mp3";
+keyaud[3].src = "audio/g-3.mp3";
+keyaud[4].src = "audio/a-3.mp3";
+keyaud[5].src = "audio/c3.mp3";
+keyaud[6].src = "audio/d3.mp3";
+keyaud[7].src = "audio/e3.mp3";
+keyaud[8].src = "audio/f3.mp3";
+keyaud[9].src = "audio/g3.mp3";
+keyaud[10].src = "audio/a4.mp3";
+keyaud[11].src = "audio/b3.mp3";
 
 for (let i = 0; i < 12; i++) {
   keys[i].addEventListener("click", (e) => {
